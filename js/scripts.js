135bf4839f6b9dc58ba1123af36ee21aaffaa90a
wedding-hyunguk-hyemin <108382503+wedding-hyunguk-hyemin@users.noreply.github.com>
2022-09-05 21:44:28 -0500

    draft completed

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,26 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+$(function (){
+	$("#btn_toggle").click(function (){
+  	$("#divToggle").toggle();
+  });
+});
+
+function copyFunction1() {
+    navigator.clipboard.writeText("3020642167271");
+    alert("계좌번호가 복사되었습니다");
+}
+
+function copyFunction2() {
+    navigator.clipboard.writeText("123458790");
+    alert("계좌번호가 복사되었습니다");
+}
+
+
+function copyFunction3() {
+    navigator.clipboard.writeText("123458790");
+    alert("계좌번호가 복사되었습니다");
+}
+
